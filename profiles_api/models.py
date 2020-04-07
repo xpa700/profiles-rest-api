98642eda60ee3f17ba14dd2ce8c3a01cecc7880a
Abdoulaye Thiam <xpa700@hotmail.com>
2020-04-07 22:11:42 +0200

    Added GET, PUT, PATCH, and DELETE to our API View

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -9,30 +9,34 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """ Manager for user profiles """
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """ Create a new user profile """
+        """Create a new user profile"""
         if not email:
-            raise ValueError('User must have an email address')
-        email = email.normalize_email(email)
-        user = self.model(email=email,name=name)
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
+
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, name, password):
-        """ Create a new super user with given details"""
-        user = self.create(email, name, password)
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
         user.is_superuser = True
         user.is_staff = True
-        ser.save(using=self._db)
+        user.save(using=self._db)
+
         return user
 
 
-class UserProfile(AbstractBaseUser,PermissionsMixin):
-    """ Database model for the users in the system """
-
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -44,13 +48,13 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """ Retrive user full name """
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        """ Retrive user full name """
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        """ return a string representation of our user """
+        """Return string representation of user"""
         return self.email
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
+from rest_framework.views import status
+
+from profiles_api import serializers
+
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    #function that will return the API results
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditioanl djagno view',
+            'Gives you the most control over your app logic',
+            'Is mapped manually to URLs',
+        ]
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        # Let's check that the data is valid as per our seraliser validation function
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                #Here we are customising the http response send. By default: 200
+                status=status.HTTP_400_BAD_REQUEST
+                )
+
+    def put(self, request, pk=None):
+        """Handles a full update of an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handles a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
