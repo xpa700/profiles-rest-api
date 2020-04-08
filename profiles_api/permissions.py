2f1b53a2019005f4ed7971c1a07169f74352e483
Abdoulaye Thiam <xpa700@hotmail.com>
2020-04-08 18:31:09 +0200

    Configured for deployment!

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -6,10 +6,23 @@ class UpdateOwnProfile(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
-        # The SAFE_METHODS are read methods. Here we are checking
+        # 1: The SAFE_METHODS are read methods. Here we are checking
         # if the user is just trying to read his own data
+        # 2: Apparently the create method is also a safe methog
+        # Only the methods that alter and delete existing data are not
         if request.method in permissions.SAFE_METHODS:
             return True
 
         # Otherwise, check if the user is trying to update his own profile
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=tc_n48sh(*ndkfl07%s)rje==(%w@ogt+y%+c08=i=f6db__y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
